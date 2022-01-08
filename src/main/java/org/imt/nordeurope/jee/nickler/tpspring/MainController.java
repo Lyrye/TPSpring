fa6818fe8056296e15626ac202bd4189c6766775
Lila Nickler <lila.nickler@gmail.com>
2022-01-08 16:30:23 +0100

    Factorisation

--- src/main/java/org/imt/nordeurope/jee/nickler/tpspring/MainController.java
+++ src/main/java/org/imt/nordeurope/jee/nickler/tpspring/MainController.java
@@ -15,9 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
     public static ApplicationContext applicationContext;
 
-    //@Autowired
-    public AnnotationTemperatureMain annotationTemperatureMain;
-
+    @Autowired
+    AnnotationTemperatureMain annotationTemperatureMain;
 
     static {
         applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
@@ -31,27 +30,25 @@ public class MainController {
     @RequestMapping(value = {"/temperatures" }, method = RequestMethod.GET)
     public String temperatures(Model model) {
         TemperatureMain bean = applicationContext.getBean(TemperatureMain.class);
-        model.addAttribute("temperatureFR",bean.frBean.getTemperature());
-        model.addAttribute("temperatureDE",bean.deBean.getTemperature());
-        model.addAttribute("temperatureEN",bean.enBean.getTemperature());
-
-        model.addAttribute("hashCodeFR",bean.frBean.getHash());
-        model.addAttribute("hashCodeDE",bean.deBean.getHash());
-        model.addAttribute("hashCodeEN",bean.enBean.getHash());
+        setAttribute(model, bean.frBean, bean.deBean,bean.enBean);
         return "temperatures";
     }
 
     @RequestMapping(value = {"/temperaturesAnnotation" }, method = RequestMethod.GET)
     public String temperaturesAnnotation(Model model) {
-        
-        model.addAttribute("temperatureFR",annotationTemperatureMain.frBean.getTemperature());
-        model.addAttribute("temperatureDE",annotationTemperatureMain.deBean.getTemperature());
-        model.addAttribute("temperatureEN",annotationTemperatureMain.enBean.getTemperature());
 
-        model.addAttribute("hashCodeFR",annotationTemperatureMain.frBean.getHash());
-        model.addAttribute("hashCodeDE",annotationTemperatureMain.deBean.getHash());
-        model.addAttribute("hashCodeEN",annotationTemperatureMain.enBean.getHash());
+        setAttribute(model,annotationTemperatureMain.frBean, annotationTemperatureMain.deBean, annotationTemperatureMain.enBean);
         return "temperatures";
+    }
+
+    private void setAttribute(Model model, TemperatureBean frBean,TemperatureBean deBean,TemperatureBean enBean) {
+        model.addAttribute("temperatureFR", frBean.getTemperature());
+        model.addAttribute("temperatureDE", deBean.getTemperature());
+        model.addAttribute("temperatureEN", enBean.getTemperature());
+
+        model.addAttribute("hashCodeFR", frBean.getHash());
+        model.addAttribute("hashCodeDE", deBean.getHash());
+        model.addAttribute("hashCodeEN", enBean.getHash());
     }
 
 }
